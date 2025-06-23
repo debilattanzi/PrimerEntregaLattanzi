a9db10b8f934bb2f79d01dcedd8cc1ba7cdb3e3d
Debi Lattanzi <deboralattanzi@hotmail.com>
2025-06-22 21:46:35 -0300

    creacion archivo JSON

--- CartManager.js
+++ CartManager.js
@@ -1,12 +1,35 @@
+const fs = require('fs');
+
+const ruteo = './carts.json';
 
 
-
-let carritos = [];
 
 class CartManager {
     constructor() {
-        this.carritos = carritos;
-        this.nextId = 1; 
+        this.ruteo = ruteo;
+        this.carritos = this.leerCarritos();
+        this.nextId = this.carritos.length > 0 ? this.carritos[this.carritos.length - 1].id + 1 : 1;
+
+        //this.carritos = carritos;
+        //this.nextId = 1; 
+    }
+
+    leerCarritos() {
+        try {
+            const data = fs.readFileSync(this.ruteo, 'utf8');
+            return JSON.parse(data);
+        } catch (error) {
+            console.error('Error cargando carritos:', error);
+            return [];
+        }
+    }
+
+    guardarCarritos() {
+        fs.writeFileSync(this.ruteo, JSON.stringify(this.carritos, null, 2));
+    }
+
+    getCarritos() {
+        return this.carritos.map(c => ({ ...c, products: c.products.map(p => ({ ...p, product: this.productManager.getProduct(p.id) })) }));
     }
 
     getCarts() {
@@ -14,16 +37,25 @@ class CartManager {
     }
 
     getCart(id) {
-        return this.carritos.find(carrito => carrito.id === id);
+        return this.carritos.find(c => c.id === parseInt(id));
     }
 
     addCart(products) {
-        const nuevoCarrito = {
-            id: this.nextId++,
-            products
-        };
+        const newId = this.carritos.length > 0 ? this.carritos[this.carritos.length - 1].id + 1 : 1;
+        const nuevoCarrito = { id: newId, products };
         this.carritos.push(nuevoCarrito);
+        this.guardarCarritos();
         return nuevoCarrito;
+    }
+
+    updateCart(id, updatedFields) {
+        const index = this.carritos.findIndex(c => c.id === id);
+        if (index !== -1) {
+            this.carritos[index] = { ...this.carritos[index], ...updatedFields, id }; 
+            this.guardarCarritos();
+            return this.carritos[index];
+        }
+        return null;
     }
 
 }
